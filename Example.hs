e503f7336057e2130f2e779220c2f931980f4e05
ners <ners@gmx.ch>
2024-03-29 17:27:14 +0100

    more interesting example :)

--- Example.hs
+++ Example.hs
@@ -1,25 +1,89 @@
 module Example where
 
+import Data.Vector.Storable (Vector)
+import Data.Vector.Storable qualified as Vector
 import FRP.Rhine hiding (EventClock)
-import FRP.Rhine.SDL qualified as SDL
+import FRP.Rhine.SDL (flowSDL)
 import SDL qualified
+import SDL.Primitive qualified as SDL
 import System.Exit (exitSuccess)
+import System.Random (randomIO)
 import Prelude
 
-type State = ()
+data State = State
+    { sides :: Int
+    , radius :: Double
+    , angle :: Double
+    , velocity :: Double
+    , colour :: SDL.Color
+    }
 
-handleEvent :: forall m. (MonadIO m) => ClSF m SDL.EventClock State State
-handleEvent = tagS &&& returnA >>> arrMCl (uncurry h)
-  where
-    h :: SDL.Event -> State -> m State
-    h (SDL.eventPayload -> SDL.QuitEvent) = const $ liftIO exitSuccess
-    h _ = pure
+handleEvent :: (MonadIO m, Tag cl ~ SDL.Event) => ClSF m cl State State
+handleEvent =
+    tagS &&& returnA >>> arrMCl \(e, st) -> do
+        case e.eventPayload of
+            SDL.QuitEvent -> liftIO exitSuccess -- TODO: use ExceptT instead
+            SDL.MouseButtonEvent (SDL.MouseButtonEventData{..}) | mouseButtonEventMotion == SDL.Pressed -> do
+                (r, b, g) <- randomIO
+                pure st{colour = SDL.V4 r g b 255}
+            SDL.MouseWheelEvent
+                (SDL.MouseWheelEventData{mouseWheelEventPos = SDL.V2 _ direction}) ->
+                    pure
+                        st
+                            { radius =
+                                max 1 $ st.radius + st.radius * fromIntegral direction * 0.1
+                            }
+            SDL.KeyboardEvent
+                ( SDL.KeyboardEventData
+                        { keyboardEventKeyMotion = SDL.Pressed
+                        , keyboardEventRepeat
+                        , keyboardEventKeysym =
+                            SDL.Keysym{keysymScancode = SDL.Scancode{unwrapScancode = code}}
+                        }
+                    )
+                    | code == 79 -> pure st{velocity = st.velocity + 0.5}
+                    | code == 80 -> pure st{velocity = st.velocity - 0.5}
+                    | code == 81 && not keyboardEventRepeat ->
+                        pure st{sides = max 3 $ st.sides - 1}
+                    | code == 82 && not keyboardEventRepeat -> pure st{sides = st.sides + 1}
+            _ -> pure st
 
-simulate :: forall m. (MonadIO m) => ClSF m SDL.SimulationClock State State
-simulate = returnA
+simulate :: (MonadIO m, Time cl ~ UTCTime) => ClSF m cl State State
+simulate =
+    sinceLastS &&& returnA >>^ \(dt, st) -> st{angle = st.angle + st.velocity * dt}
 
-renderFrame :: forall m. (MonadIO m) => ClSF m SDL.RenderClock State ()
-renderFrame = returnA
+renderFrame :: (MonadIO m) => ClSF m cl (State, SDL.Renderer, SDL.Window) ()
+renderFrame = arrMCl \(State{..}, renderer, window) -> do
+    (w, h) <-
+        (\(SDL.V2 w h) -> (fromIntegral w, fromIntegral h))
+            <$> SDL.get (SDL.windowSize window)
+    let tau :: (Floating a) => a
+        tau = 2 * pi
+        pointAngle :: Int -> Double
+        pointAngle i = angle + fromIntegral i * tau / fromIntegral sides
+        x, y :: Int -> Double
+        x i = radius * cos (pointAngle i) + w / 2
+        y i = radius * sin (pointAngle i) + h / 2
+        xs, ys :: (Integral a, Vector.Storable a) => Vector a
+        xs = Vector.generate sides $ round . x
+        ys = Vector.generate sides $ round . y
+    SDL.fillPolygon renderer xs ys colour
+
+type SimClock = Millisecond 10
+
+-- 60 FPS => 1000/60 ms/frame
+type RenderClock = Millisecond 16
 
 main :: IO ()
-main = SDL.flowSDL () handleEvent simulate renderFrame
+main =
+    flowSDL @IO @SimClock @RenderClock
+        State
+            { sides = 3
+            , radius = 200
+            , angle = 0
+            , velocity = 1
+            , colour = SDL.V4 255 255 255 255
+            }
+        handleEvent
+        simulate
+        renderFrame
--- src/FRP/Rhine/SDL.hs
+++ src/FRP/Rhine/SDL.hs
@@ -6,9 +6,11 @@ import Control.Monad.Schedule.Class (MonadSchedule)
 import Data.Time (addUTCTime, getCurrentTime, secondsToNominalDiffTime)
 import Data.Word (Word32)
 import FRP.Rhine hiding (EventClock)
+import SDL (($=))
 import SDL qualified
 import SDL.Raw qualified
 import Prelude
+import GHC.TypeNats (KnownNat)
 
 instance TimeDifference Word32 where
     add = (+)
@@ -41,51 +43,56 @@ instance (MonadIO m) => Clock m EventClock where
 
 instance GetClockProxy EventClock
 
-type SimulationClock = Millisecond 10
-
-type FooClock = ParallelClock EventClock SimulationClock
-
--- | 60 FPS ~ 16 ms/frame
-type RenderClock = Millisecond 16
-
 flowSDL
-    :: forall m st
+    :: forall m simCl renderCl sim render cl st
      . ( MonadIO m
        , MonadSchedule m
-       , Clock m SimulationClock
-       , Clock m RenderClock
+       , Clock m simCl
+       , Clock m renderCl
+       , KnownNat sim
+       , simCl ~ Millisecond sim
+       , KnownNat render
+       , renderCl ~ Millisecond render
+       , cl ~ ParallelClock EventClock simCl
        )
     => st
     -> ClSF m EventClock st st
-    -> ClSF m SimulationClock st st
-    -> ClSF m RenderClock st ()
+    -> ClSF m simCl st st
+    -> ClSF m renderCl (st, SDL.Renderer, SDL.Window) ()
     -> m ()
 flowSDL initialState handleEvent simulate renderFrame = do
     SDL.initializeAll
-    window <- SDL.createWindow "Title" SDL.defaultWindow
+    window <- SDL.createWindow "Title" SDL.defaultWindow{SDL.windowResizable = True}
     renderer <- SDL.createRenderer window (-1) SDL.defaultRenderer
-    let renderRh :: Rhine m RenderClock st ()
-        renderRh = render renderer @@ waitClock
+    let renderRh :: Rhine m renderCl (st, SDL.Renderer, SDL.Window) ()
+        renderRh = render @@ waitClock
     flow $
         handleEventOrSim
             >-- keepLast initialState
-            --> renderRh
+            --> (,renderer,window)
+                ^>>@ renderRh
     SDL.destroyWindow window
     SDL.quit
   where
     eventRh :: Rhine m EventClock st st
     eventRh = handleEvent @@ EventClock
-    simRh :: Rhine m SimulationClock st st
+    simRh :: Rhine m simCl st st
     simRh = simulate @@ waitClock
-    handleEventOrSim :: Rhine m FooClock () st
+    handleEventOrSim :: Rhine m cl () st
     handleEventOrSim =
         feedbackRhine
             (keepLast initialState)
             (feedbackify eventRh |@| feedbackify simRh)
-    feedbackify :: Rhine m cl a a -> Rhine m cl ((), a) (a, a)
+    feedbackify :: Rhine m cl' a a -> Rhine m cl' ((), a) (a, a)
     feedbackify rh = snd ^>>@ rh @>>^ (\st -> (st, st))
-    render :: SDL.Renderer -> ClSF m RenderClock st ()
-    render renderer =
-        renderFrame >>> constM do
-            SDL.clear renderer
-            SDL.present renderer
+    preRender, postRender :: SDL.Renderer -> m ()
+    preRender renderer = do
+        SDL.rendererDrawColor renderer $= SDL.V4 0 0 0 255
+        SDL.clear renderer
+    postRender renderer = do
+        SDL.present renderer
+    render :: ClSF m renderCl (st, SDL.Renderer, SDL.Window) ()
+    render = proc x@(_, renderer, _) -> do
+        arrMCl preRender -< renderer
+        renderFrame -< x
+        arrMCl postRender -< renderer
